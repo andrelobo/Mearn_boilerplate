f121c9ad1897333374d7c2c6c222becae5412a74
Andre Lobo <loboandre@hotmail.com>
2023-08-31 15:01:44 -0400

    remove gitignore files

--- server/config.js
+++
@@ -1,7 +0,0 @@
-export default  {
-    JWT_SECRET : "nhaca",
-    EMAIL: "xonga73@gmail.com", // testing email & password
-    PASSWORD : "Democracia70x07@",
-    ATLAS_URI: "mongodb+srv://lobo:nhaca70x07@cluster0.fpngzuh.mongodb.net/?retryWrites=true&w=majority"
-}
-
